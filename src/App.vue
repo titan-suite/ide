89f3f53212424ae6e6a24c8fc2ca110ba3c6c72b
Tomisin <tomsiin@northernblock.ca>
2018-11-06 15:16:21 -0500

    Added code editor component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <Editor msg="Welcome to Titan" />
+    <Editor :code="code" :cmOptions="cmOptions" />
   </div>
 </template>
 
@@ -11,16 +11,28 @@ export default {
   name: 'app',
   components: {
     Editor
+  },
+  data() {
+  return {
+    code: `const jokes = "Welcome to Titan's IDe | the #1 mofo ide for mofo hardcore devs";`,
+    cmOptions: {
+      tabSize: 4,
+      mode: 'text/javascript',
+      theme: 'monokai',
+      lineNumbers: true,
+      line: true,
+      gutters: ['CodeMirror-linenumbers', 'breakpoints']
+    }
+  }
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,21 +1,64 @@
 <template>
-    <div class="hello">
-        <el-button>{{ msg }}</el-button>
+    <div class="main">
+        <codemirror ref="myCm" :value="code" :options="cmOptions" @ready="onCmReady" @focus="onCmFocus" @input="onCmCodeChange" @gutterClick="onGutterClick">
+        </codemirror>
     </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator'
+import { codemirror } from 'vue-codemirror'
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/theme/monokai.css'
+import 'codemirror/mode/javascript/javascript.js'
 
-@Component
+@Component({
+    components: {
+        codemirror
+    }
+})
+
 export default class Editor extends Vue {
-  @Prop() private msg!: string
+    @Prop() private code!: string
+    @Prop() private cmOptions!: {}
+
+    public mounted(): void {
+        const { codemirror: codemirrorRef }: any = this.$refs.myCm
+        console.log('this is current codemirror object', codemirrorRef)
+        console.log(this.code, this.cmOptions)
+        // you can use this.codemirror to do something...
+    }
+
+    public onCmReady(cm: any) {
+      console.log('the editor is readied!', cm)
+    }
+
+    public onCmFocus(cm: any) {
+      console.log('the editor is focus!', cm)
+    }
+
+    public onCmCodeChange(code: any) {
+      console.log('this is new code', code)
+      this.code = code
+    }
+    
+    public onGutterClick(cm: any, n: any) {
+        const info = cm.lineInfo(n)
+        console.log(info)
+        cm.setGutterMarker(n, 'breakpoints', info.gutterMarkers ? null : this.makeMarker())
+    }
+
+    public makeMarker() {
+        const marker = document.createElement('div')
+        marker.style.color = '#fff'
+        marker.innerHTML = '●'
+        return marker
+    }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="stylus">
-.hello {
+.main {
     margin: 40px 0 0;
 }
 </style>
