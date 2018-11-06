a82407257f8d8898c592f51d09791e776f8f3a2d
Arjit Khullar <arjitkhullar@gmail.com>
2018-11-06 13:47:14 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Editor msg="Welcome to Titan" />
+  </div>
+</template>
+
+<script>
+import Editor from './components/Editor.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Editor
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Editor.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="hello">
+        <el-button>{{ msg }}</el-button>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component
+export default class Editor extends Vue {
+  @Prop() private msg!: string
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="stylus">
+.hello {
+    margin: 40px 0 0;
+}
+</style>
