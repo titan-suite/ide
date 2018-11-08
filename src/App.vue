84cc47cd1fb0e561ecfdf467915f18dcaefd0222
Arjit Khullar <arjitkhullar@gmail.com>
2018-11-08 10:54:46 -0500

    added draggable containers

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,25 @@
 <template>
   <el-container class="main">
-    <el-aside width="200px">
-      <FileExplorer />
+    <el-aside width="auto">
+      <vue-draggable-resizable style=" height: 100%;margin-right:16px; border: 1px solid red; position: relative;" :draggable="false" axis="x" :handles="['mr']">
+        <FileExplorer />
+      </vue-draggable-resizable>
     </el-aside>
     <el-container>
       <el-header height="10px">Header</el-header>
       <el-main>
         <Editor :code="code" :cmOptions="cmOptions" />
       </el-main>
-      <el-footer>
-        <Console />
+      <el-footer height="auto">
+        <vue-draggable-resizable style="width: 100%; border: 1px solid red; position: relative;" :draggable="false" axis="y" :handles="['tm']">
+          <Console />
+        </vue-draggable-resizable>
       </el-footer>
     </el-container>
-    <el-aside width="30%">
-      <Sidebar />
+    <el-aside width="auto">
+      <vue-draggable-resizable style=" height: 100%;left: 1rem; margin-right:16px; border: 1px solid red; position: relative;" :draggable="false" axis="x" :handles="['ml']">
+        <Sidebar />
+      </vue-draggable-resizable>
     </el-aside>
   </el-container>
 </template>
@@ -23,9 +29,11 @@ import Editor from './components/Editor.vue'
 import Console from './components/Console.vue'
 import FileExplorer from './components/FileExplorer.vue'
 import Sidebar from './components/Sidebar/Index.vue'
+import VueDraggableResizable from 'vue-draggable-resizable'
 export default {
   name: 'app',
   components: {
+    VueDraggableResizable,
     Editor,
     Console,
     FileExplorer,
@@ -48,6 +56,7 @@ export default {
 </script>
 
 <style lang="stylus">
-.main
-  font:12px/normal 'Monaco', 'Menlo', 'Ubuntu Mono', 'Consolas', 'source-code-pro', monospace
+.main {
+  font: 12px / normal 'Monaco', 'Menlo', 'Ubuntu Mono', 'Consolas', 'source-code-pro', monospace;
+}
 </style>
--- src/components/Sidebar/Index.vue
+++ src/components/Sidebar/Index.vue
@@ -1,19 +1,19 @@
 <template>
     <el-tabs v-model="activeName">
         <el-tab-pane label="Compile" name="first">
-            <Compile/>
+            <Compile />
         </el-tab-pane>
         <el-tab-pane label="Run" name="second">
-            <Run/>
+            <Run />
         </el-tab-pane>
         <el-tab-pane label="Analysis" name="third">
-            <Analysis/>
+            <Analysis />
         </el-tab-pane>
         <el-tab-pane label="Chat" name="fourth">
-            <Chat/>
+            <Chat />
         </el-tab-pane>
         <el-tab-pane label="Settings" name="fifth">
-            <Settings/>
+            <Settings />
         </el-tab-pane>
     </el-tabs>
 </template>
